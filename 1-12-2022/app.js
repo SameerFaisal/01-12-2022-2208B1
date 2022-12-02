f4ed8f5e40b2fa669193e833a1f39f3b6ce60db3
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-02 11:11:35 +0500

    Code for 01-12-2022

---
+++ 1-12-2022/app.js
@@ -0,0 +1,31 @@
+// var name="Sameer"
+
+// alert(name)
+
+// //"Sameer"
+
+// var info="Sameer"
+
+// alert(info)
+
+
+// var num1="25"
+// var num2="30"
+// document.write("Sameer"+"<h1>"+"Faisal"+"</h1>")
+
+// var number1=1
+// var number2=3
+
+// var result=number1/number2
+// document.write(parseInt(number1)-parseInt(number2))
+// document.write(parseInt(number1)*parseInt(number2))
+// document.write(parseInt(number1)/parseInt(number2))
+// document.write(result.toFixed(3))
+
+// var num=100
+
+// var result=num/3
+
+var percent=98/100*100
+
+document.write("Percentage: "+percent+"%")
